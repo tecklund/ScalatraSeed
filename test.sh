49158711916fd10b030627fc8ed27a2cf5308734
tim <tim.ecklund@smartrac-group.com>
2017-04-20 16:51:23 -0400

    update test.sh

--- test.sh
+++ test.sh
@@ -1,17 +1,3 @@
 #! /bin/bash
 
-#locale
-#uname -a
-#mongod --version
-
-#id
-#ls -al /data/
-
-
-mongod --fork --logpath mongod.log
-
-#sleep 4
-
-#cat mongod.log 
-
-./gradlew clean test -Dspecs2.console "$@" #--debug
+gradle clean test -Dspecs2.console "$@"
