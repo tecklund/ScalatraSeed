5917b63d0eac15d2b5eca86d910cc74dd857ce40
anthonygabriele <anthonyjoeseph@gmail.com>
2017-06-04 01:00:29 -0400

    do system prop based config

--- src/main/scala/org/blinkmob/scalatraseed/DB.scala
+++ src/main/scala/org/blinkmob/scalatraseed/DB.scala
@@ -4,12 +4,24 @@ import org.blinkmob.CxProvider
 import org.blinkmob.hasDataSource
 import com.zaxxer.hikari.HikariDataSource
 import com.zaxxer.hikari.HikariConfig
+import scala.util.Properties
+import com.typesafe.config.ConfigFactory
+import org.blinkmob.scalatraseed.utils.Logging
 
-object DB extends CxProvider with hasDataSource{
+
+object DB extends CxProvider with hasDataSource with Logging{
+  
+  val conf = ConfigFactory.load()
+  val env = conf.getString("env")
+  
+  val dbUrl = conf.getString(s"${env}.dbUrl")
+  val dbUsername = conf.getString(s"${env}.dbUser")
+  val dbPassword = conf.getString(s"${env}.dbPassword")
   
   var config = new HikariConfig();
-  config.setJdbcUrl("jdbc:h2:file:./target/foobar");
-  config.setUsername("sa")
+  config.setJdbcUrl(dbUrl)
+  config.setUsername(dbUsername)
+  config.setPassword(dbPassword)
   
   val ds = new HikariDataSource(config)  
 }
